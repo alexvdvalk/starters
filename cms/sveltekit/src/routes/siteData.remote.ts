8f7c8419b4c2c3aa06640051f4504f4c48e76ece
Alex van der Valk <21194068+alexvdvalk@users.noreply.github.com>
2026-04-15 15:22:12 +0200

    fix .env

--- cms/sveltekit/src/routes/siteData.remote.ts
+++ cms/sveltekit/src/routes/siteData.remote.ts
@@ -1,6 +1,8 @@
 import { query, getRequestEvent } from "$app/server";
-import { PUBLIC_ENABLE_VISUAL_EDITING } from "$env/static/public";
 import { fetchSiteData } from "$lib/directus/fetchers";
+
+import { env } from '$env/dynamic/public';
+
 
 export const getSiteData = query(async () => {
     console.log("getting site data")
@@ -8,7 +10,7 @@ export const getSiteData = query(async () => {
     // Enabled by default; set to 'false' to disable
     const visualEditingEnabled =
         event.url.searchParams.get('visual-editing') === 'true' &&
-        PUBLIC_ENABLE_VISUAL_EDITING !== 'false';
+        env.PUBLIC_ENABLE_VISUAL_EDITING !== 'false';
     const { globals, headerNavigation, footerNavigation } = await fetchSiteData();
     const accentColor = globals?.accent_color || '#6644ff';
     return { globals, headerNavigation, footerNavigation, accentColor, visualEditingEnabled };
